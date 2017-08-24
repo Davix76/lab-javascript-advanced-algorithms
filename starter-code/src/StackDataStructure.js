35720994d7f66c3523abeb8e104a03a47c241498
Davix76 <30471850+Davix76@users.noreply.github.com>
2017-08-24 10:04:02 +0200

    lab David no terminado

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,35 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
 }
+
+StackDataStructure.prototype.isEmpty = function() {
+  // return this.stackControl.length < 1 ? true : false;
+  return this.stackControl.length < 1;
+};
+
+StackDataStructure.prototype.canPush = function() {
+  return this.MAX_SIZE > this.stackControl.length;
+  // if (this.MAX_SIZE > this.stackControl.length){
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+};
+
+StackDataStructure.prototype.push = function(element) {
+  this.stackControl.push(element);
+  if (this.MAX_SIZE > this.stackControl.length) {
+    return this.stackControl;
+  } else {
+    return ("Stack Overflow");
+  }
+};
+
+StackDataStructure.prototype.pop = function() {
+  if (this.stackControl.length === 0) {
+    return "Stack Underflow";
+  } else {
+    return this.stackControl.pop();
+  }
+};
